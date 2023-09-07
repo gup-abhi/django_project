69b07be6f3fe45e6228085fd4393d8c5c2369537
Abhishek Gupta <agabhishek20121998@gmail.com>
2023-09-07 00:56:41 -0400

    Updated static url in setting and url

--- django_project/urls.py
+++ django_project/urls.py
@@ -48,8 +48,7 @@ urlpatterns = [
          ),
          name='password_reset_complete'),
     path('', include('blog.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL , document_root=settings.STATIC_ROOT)
